303a91153cc86349bfb1128dc699824a00e28e71
Vladimir Muromskiy <aiditz@yandex.ru>
2019-06-08 11:28:07 +0300

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,23 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prefer-template': 'off',
+    'operator-linebreak': 'off',
+    'space-infix-ops': 'off',
+    'no-await-in-loop': 'off',
+    'arrow-parens': 'off',
+    'arrow-body-style': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'max-len': ['error', {
+      code: 120,
+      ignoreUrls: true
+    }],
+    'comma-dangle': 'off',
+    'func-names': 'off',
+    'object-curly-spacing': 'off',
+    'object-curly-newline': 'off',
+    'prefer-destructuring': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
