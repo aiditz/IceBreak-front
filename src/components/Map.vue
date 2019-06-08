72aa8ca050aa0052554ae0d707472c34a2aba21f
Vladimir Muromskiy <aiditz@yandex.ru>
2019-06-08 11:21:28 +0300

    Add svgs

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
-    <div class="layer background"></div>
-    <div class="layer paths"></div>
-    <div class="layer objects"></div>
-  </div>
+  <svg id="map" viewBox="0 0 640 360">
+      <BackgroundLayer></BackgroundLayer>
+  </svg>
 </template>
 
 <script>
-export default {
-  name: 'IceBreak',
-  props: {
-  },
-};
+  import BackgroundLayer from './map/background.svg.vue';
+
+  export default {
+    name: 'IceBreak',
+    components: {
+      BackgroundLayer
+    },
+    props: {
+    },
+  };
 </script>
 
 <style scoped>
+  #map {
+    width: calc(24 * 20px);
+    height: calc(13 * 2/3 * 1.414213562373095 * 20px);
+    position: absolute;
+  }
 </style>
