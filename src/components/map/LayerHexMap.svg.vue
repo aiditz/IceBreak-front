ead7101575fc208a9d8b734d4ac55fbfa173da09
margolin96 <margolin96@gmail.com>
2019-06-08 13:14:35 +0300

    On click console log row & col index

--- src/components/map/LayerHexMap.svg.vue
+++ src/components/map/LayerHexMap.svg.vue
@@ -1,7 +1,16 @@
 <template>
-  <g>
-    <g v-for="(row, rowIndex) in gs.colors" :transform="'translate(' + getRowTranslate(rowIndex) + ')'">
-      <Hexagon v-for="(item, itemIndex) in row" :transform="'translate(' + getItemTranslate(itemIndex) + ' 0)'" :color="item"></Hexagon>
+  <g class="hexGird"
+    :transform="'translate(' + greadTranslate + ')'"
+  >
+    <g class="hexClass" v-for="(row, rowIndex) in gs.colors"
+      :transform="'translate(' + getRowTranslate(rowIndex) + ')'"
+    >
+      <Hexagon class="hexTile"
+        v-for="(item, itemIndex) in row"
+        :transform="'translate(' + getItemTranslate(itemIndex) + ' 0)'"
+        :color="item"
+        @click.native="tileClicked(rowIndex, itemIndex)"
+      ></Hexagon>
     </g>
   </g>
 </template>
@@ -12,8 +21,11 @@
   export default {
     name: 'IceBreak',
     computed: {
-      colors() {
+      gs() {
         return this.$store.state.gs;
+      },
+      greadTranslate() {
+        return '-50, -5';
       }
     },
     data: () => {
@@ -40,10 +52,23 @@
         }
 
         return `${x} ${y}`;
+      },
+
+      tileClicked(rowIndex, itemIndex) {
+        console.log(rowIndex, itemIndex);
       }
     }
   };
 </script>
 
 <style scoped>
+
+.hexTile {
+  transition: .15s;
+  opacity: .5;
+}
+.hexTile:hover {
+  opacity: .9;
+}
+
 </style>
