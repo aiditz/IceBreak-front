e45aa96374ce060ab46e7e975f5c80fe01fce35c
margolin96 <margolin96@gmail.com>
2019-06-08 13:24:11 +0300

    Rand colors (temp)

--- src/components/elements/Hexagon.svg.vue
+++ src/components/elements/Hexagon.svg.vue
@@ -1,5 +1,5 @@
 <template>
-	<path :fill="color" d="M17.32050807568877 0L34.64101615137754 10L34.64101615137754 30L17.32050807568877 40L0 30L0 10Z"></path>
+	<path :fill="randColor" d="M17.32050807568877 0L34.64101615137754 10L34.64101615137754 30L17.32050807568877 40L0 30L0 10Z"></path>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ export default {
 	name: 'Hexagon',
 	data() {
 		return {}
+	},
+	computed: {
+		randColor() {
+			return '#'+Math.floor(Math.random()*16777215).toString(16);
+		}
 	},
 	props: ['color']
 }
